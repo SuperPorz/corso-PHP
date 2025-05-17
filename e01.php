ed1b74ce653a755191123843bcd78544a3ca8322
superporz <mikybeeh@hotmail.it>
2025-05-17 17:15:33 +0200

    initial commit

---
+++ e01.php
@@ -0,0 +1,8 @@
+# Scrivere una coppia di file (HTML e PHP) dove l'HTML contiene un form nel quale l'utente può inserire un numero di stanza; 
+# il PHP deve contenere un array delle stanze con i relativi occupanti e il numero di telefono interno; quando l'utente manda il numero 
+# di stanza al PHP tramite il form, il PHP deve scrivere il nome dell'occupante e il relativo numero di telefono. In caso non sia passato nessun 
+# numero di stanza il PHP deve scrivere un elenco di tutti i numeri di stanza con il relativo occupante e telefono.
+
+<?php
+
+    
